60972adaef2410ed9b57f7c9d7606f9251070362
misato-takagi <3310.takagi.thanks@gmail.com>
2022-11-28 16:12:10 +0900

    env

---
+++ project/sample_local_settings.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import os
+BASE_DIR = Path(__file__).resolve().parent.parent
+# SECRET_KEY =  # 各自調べて設定 (https://chigusa-web.com/blog/django-secret/)
+# DATABASES = {
+#     'default': {
+#         'ENGINE':  # 各自環境に合わせて設定,
+#         'NAME': # 各自環境に合わせて設定,
+#     }
+# }
