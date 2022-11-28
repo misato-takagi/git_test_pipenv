89382f49d8956459515e573bbb75b17c1a20f208
misato-takagi <3310.takagi.thanks@gmail.com>
2022-11-28 16:44:36 +0900

    asdfasdf

--- project/app1/views.py
+++ project/app1/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.views.generic import DeleteView
 # Create your views here.
+
+class DeleteHosView(DeleteView):
+    template_name: str = 'delete.html'
